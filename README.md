4e4320bdb5b7829260eb0e0dcaf5d175007ebb32
Phil Schanely <philschanely@gmail.com>
2020-04-20 15:30:03 -0400

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# ps-product-tour-sandbox-2
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import "./styles.scss";
+
+var data = {
+  items: [
+    {
+      title: "Page 1",
+      target: ""
+    },
+    {
+      title: "Page 2",
+      target: ""
+    },
+    {
+      title: "Page 3",
+      target: ""
+    },
+    {
+      title: "Page 4",
+      target: ""
+    },
+    {
+      title: "Page 5",
+      target: ""
+    }
+  ]
+};
+
+function init() {}
+
+init();
